a58e8949c25df5efb9c5801d5fa0cdcb82f0c0c8
Mike Hsu <myhsu@benjaminelectric.com>
2017-08-25 09:33:58 -0700

    nevermind doesn't work in production

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,6 @@
-require('babel-register')({
+/* require('babel-register')({
   "presets": ["electron"],
   ignore: /autoupdate.js/
-});
+}); */
 require('json5/lib/require');
 require('./main/index');
