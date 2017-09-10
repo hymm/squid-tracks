dbba38270781eeeaf9334474c3cf4186fec596ab
Mike Hsu <mike.hsu@gmail.com>
2017-09-10 00:17:23 -0700

    upload all is working

--- src/components/results-upload-all-battles.js
+++ src/components/results-upload-all-battles.js
@@ -5,8 +5,12 @@ import { ipcRenderer } from 'electron';
 import { event } from '../analytics';
 
 class UploadAllBattlesButton extends React.Component {
+  state = {
+    uploading: false,
+    currentIdx: 0
+  };
+
   componentDidMount() {
-    console.log(this.props);
     ipcRenderer.on('wroteBattleAll', this.handleWroteBattle);
     ipcRenderer.on('writeBattleAllError', this.handleError);
   }
@@ -17,26 +21,57 @@ class UploadAllBattlesButton extends React.Component {
   }
 
   uploadAllBattles = () => {
-    const { splatnet, statInk } = this.props;
+    this.initializeUpload();
+    const startIdx = this.props.splatnet.current.results.results.length - 1;
+    this.uploadBattle(startIdx);
+  };
 
-    for (const battle of splatnet.current.results.results) {
-      const uploaded = statInk ? statInk[battle.battle_number] != null : false;
-      if (!uploaded) {
-        const battleDetails = splatnet.comm.getBattle(battle.battle_number);
-        ipcRenderer.send('writeToStatInk', battleDetails, 'all');
-      }
+  initializeUpload = () => {
+    this.setState({ uploading: true });
+  };
+
+  uploadBattle = battleIdx => {
+    const { splatnet, statInk } = this.props;
+    this.setState({ currentIdx: battleIdx });
+    const battle = this.props.splatnet.current.results.results[battleIdx];
+
+    const uploaded = statInk ? statInk[battle.battle_number] != null : false;
+    if (!uploaded) {
+      const battleDetails = splatnet.comm.getBattle(battle.battle_number);
+      ipcRenderer.send('writeToStatInk', battleDetails, 'all');
+    } else {
+      setTimeout(this.uploadNext, 100);
+    }
+  };
+
+  uploadNext = () => {
+    const { uploading, currentIdx } = this.state;
+    console.log(this.state);
+    if (uploading && currentIdx - 1 >= 0) {
+      this.uploadBattle(currentIdx - 1);
+    } else {
+      this.finalizeUpload();
     }
   };
 
   handleWroteBattle = (e, info, number) => {
-    const { setStatInkInfo } = this.props;
+    const { setStatInkInfo, splatnet } = this.props;
+
     event('stat.ink', 'wrote-battle', 'auto');
     if (info.username) {
       setStatInkInfo(number, info);
     }
+
+    this.uploadNext();
   };
 
-  handleError = (e, error) => {};
+  finalizeUpload = () => {
+    this.setState({ uploading: false });
+  };
+
+  handleError = (e, error) => {
+    this.uploadNext();
+  };
 
   render() {
     return (
