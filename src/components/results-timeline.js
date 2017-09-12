d9600d45c2a78d5f88b164f3de5a26db0aeb94cb
Mike Hsu <mike.hsu@gmail.com>
2017-09-11 23:18:49 -0700

    created a decent looking chart

--- src/components/results-timeline.js
+++ src/components/results-timeline.js
@@ -4,16 +4,53 @@ import {
   ResponsiveContainer,
   ComposedChart,
   Line,
-  ReferenceArea,
+  Bar,
+  Cell,
   YAxis,
-  CartesianGrid
+  CartesianGrid,
+  ReferenceLine,
+  Dot
 } from 'recharts';
 
+class ResultDot extends React.Component {
+  render() {
+    const { payload } = this.props;
+    if (payload.result === 'victory') {
+      return <Dot {...this.props} />;
+    }
+    return null;
+  }
+}
+
 class ResultsTimeline extends React.Component {
+  getModeColor(lobby) {
+    let color = 'grey';
+    switch (lobby) {
+      case 'regular':
+        color = 'rgb(152, 207, 4)';
+        break;
+      case 'gachi':
+        color = 'rgb(249, 114, 7)';
+        break;
+      case 'league_pair':
+        color = 'rgb(228, 24, 113)';
+        break;
+      case 'league_team':
+        color = 'rgb(228, 24, 113)';
+        break;
+      case 'private':
+        color = 'rgb(157, 0, 200)';
+        break;
+      default:
+        color = 'lightgrey';
+    }
+    return color;
+  }
+
   render() {
     const results = this.props.splatnet.current.results.results;
     const data = results.map(result => {
-      let power = undefined;
+      let power = null;
       if (result.other_estimate_league_point) {
         power = result.other_estimate_league_point;
       } else if (result.other_estimate_fes_power) {
@@ -22,35 +59,31 @@ class ResultsTimeline extends React.Component {
         power = result.estimate_gachi_power;
       }
 
+      const my_count_raw =
+        result.my_team_count != null
+          ? result.my_team_count
+          : result.my_team_percentage;
+      const other_count_raw =
+        result.other_team_count != null
+          ? result.other_team_count
+          : result.other_team_percentage;
+      const scoreTotal = my_count_raw + other_count_raw;
+      const my_count = my_count_raw * 100 / scoreTotal;
+      const other_count = other_count_raw * 100 / scoreTotal;
       return {
+        bar: 100,
         power,
-        lobby: result.game_mode.key
+        lobby: result.game_mode.key,
+        result: result.my_team_result.key,
+        myScore: my_count,
+        otherScore: other_count
       };
     });
     data.reverse();
-    const modes = data.map(({ lobby }, i) => {
-      let color = 'grey';
-      switch (lobby) {
-        case 'regular':
-          color = 'lightgreen';
-          break;
-        case 'gachi':
-          color = 'orange';
-          break;
-        case 'league_pair':
-          color = 'hotpink';
-          break;
-        case 'league_team':
-          color = 'hotpink';
-          break;
-        case 'private':
-          color = 'purple';
-          break;
-        default:
-          color = 'lightgrey';
-      }
-      return <ReferenceArea x1={i} x2={i + 1} fill={color} fillOpacity={0.3} />;
-    });
+    const powerAvg = data
+      .map(d => d.power)
+      .filter(a => a != null)
+      .reduce((avg, v, i, a) => avg + v / a.length, 0);
     return (
       <ResponsiveContainer minHeight={200}>
         <ComposedChart
@@ -58,7 +91,16 @@ class ResultsTimeline extends React.Component {
           height={400}
           data={data}
           margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          barGap={0}
+          barCategoryGap={-0.5}
         >
+          <YAxis
+            yAxisId="mode"
+            dataKey="bar"
+            type="number"
+            domain={[0, 100]}
+            hide
+          />
           <YAxis
             dataKey="power"
             scale="auto"
@@ -66,8 +108,20 @@ class ResultsTimeline extends React.Component {
             domain={['dataMin', 'dataMax']}
           />
           <CartesianGrid stroke="#f5f5f5" />
-          <Line type="step" dataKey="power" stroke="#333" />
-          {modes}
+          <Bar dataKey="bar" yAxisId="mode" isAnimationActive={false}>
+            {data.map((entry, index) => {
+              const color = this.getModeColor(entry.lobby);
+              return <Cell key={index} fill={color} fillOpacity={0.8} />;
+            })}
+          </Bar>
+          <ReferenceLine y={powerAvg} stroke="white" strokeDasharray="3 3" />
+          <Line
+            type="step"
+            dataKey="power"
+            stroke="#333"
+            isAnimationActive={false}
+            dot={<ResultDot />}
+          />
         </ComposedChart>
       </ResponsiveContainer>
     );
