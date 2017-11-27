7c20c1da53700f5ad7643baa7d5e2b942ca646b1
DanSyor <daniel.vantroys@free.fr>
2017-11-27 19:07:40 +0100

    adding French translation

--- src/messages.js
+++ src/messages.js
@@ -1,9 +1,12 @@
 import { addLocaleData } from 'react-intl';
 import jaLocaleData from 'react-intl/locale-data/ja';
+import frLocaleData from 'react-intl/locale-data/fr';
 import ja from './locales/ja';
+import fr from './locales/fr';
 
-addLocaleData([...jaLocaleData]);
+addLocaleData([...jaLocaleData, ...frLocaleData]);
 
 export default {
-  ja
+  ja,
+  fr
 };
