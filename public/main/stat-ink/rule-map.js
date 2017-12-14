14508a5e9854a514f9576e46168a9bcbbbb194ca
Mike Hsu <mike.hsu@gmail.com>
2017-12-13 22:08:50 -0800

    finished adding statink support

--- public/main/stat-ink/rule-map.js
+++ public/main/stat-ink/rule-map.js
@@ -2,6 +2,6 @@ module.exports = {
   turf_war: 'nawabari',
   splat_zones: 'area',
   tower_control: 'yagura',
-  rainmaker: 'hoko'
-  clam_blitz: 'asari',
+  rainmaker: 'hoko',
+  clam_blitz: 'asari'
 };
--- public/main/stat-ink/stat-ink.js
+++ public/main/stat-ink/stat-ink.js
@@ -58,27 +58,27 @@ async function setGameInfo(statInk, result) {
 }
 
 async function setPowerInfo(statInk, result) {
-    if (result.estimate_gachi_power != null) {
-        statInk.estimate_gachi_power = result.estimate_gachi_power;
-    }
-    if (result.league_point != null) {
-        statInk.league_point = result.league_point;
-    }
-    if (result.my_estimate_league_point != null) {
-        statInk.my_team_estimate_league_point = result.my_estimate_league_point;
-    }
-    if (result.other_estimate_league_point != null) {
-        statInk.his_team_estimate_league_point = result.other_estimate_league_point;
-    }
-    if (result.fes_power != null) {
-        statInk.fest_power = result.fes_power;
-    }
-    if (result.my_estimate_fes_power != null) {
-        statInk.my_team_estimate_fes_power = result.my_estimate_fes_power;
-    }
-    if (result.other_estimate_fes_power != null) {
-        statInk.other_team_estimate_fes_power = result.other_estimate_fes_power;
-    }
+  if (result.estimate_gachi_power != null) {
+    statInk.estimate_gachi_power = result.estimate_gachi_power;
+  }
+  if (result.league_point != null) {
+    statInk.league_point = result.league_point;
+  }
+  if (result.my_estimate_league_point != null) {
+    statInk.my_team_estimate_league_point = result.my_estimate_league_point;
+  }
+  if (result.other_estimate_league_point != null) {
+    statInk.his_team_estimate_league_point = result.other_estimate_league_point;
+  }
+  if (result.fes_power != null) {
+    statInk.fest_power = result.fes_power;
+  }
+  if (result.my_estimate_fes_power != null) {
+    statInk.my_team_estimate_fes_power = result.my_estimate_fes_power;
+  }
+  if (result.other_estimate_fes_power != null) {
+    statInk.other_team_estimate_fes_power = result.other_estimate_fes_power;
+  }
 }
 
 function setGameResults(statInk, result) {
@@ -119,7 +119,7 @@ async function setPlayerResults(statInk, result) {
   statInk.level = result.player_result.player.player_rank;
   statInk.level_after = result.player_rank;
   if (result.star_rank != null) {
-      statInk.star_rank = rsult.star_rank;
+    statInk.star_rank = result.star_rank;
   }
 
   if (result.player_result.player.udemae) {
@@ -215,6 +215,7 @@ async function convertResultToStatInk(result, disableGetImage) {
   const statInk = {};
   setUuid(statInk, result);
   await setGameInfo(statInk, result);
+  setPowerInfo(statInk, result);
   setGameResults(statInk, result);
 
   await setPlayerResults(statInk, result);
